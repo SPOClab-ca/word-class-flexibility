53381beb953e3ea861fb3b90f88e13964d56ab16
Bai Li <bai@cs.toronto.edu>
2019-08-27 01:32:49 -0400

    Parse BNC corpus

--- src/corpus.py
+++ src/corpus.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 import conllu
+import nltk.corpus.reader.bnc
+import tqdm
 
 
 class POSCorpus(object):
@@ -20,6 +22,7 @@ class POSCorpus(object):
 
   @classmethod
   def create_from_ud(cls, data_file_path):
+    """Initialize corpus from a path to a file in conllu format"""
     corpus = POSCorpus()
     with open(data_file_path, "r", encoding="utf-8") as data_file:
       corpus.data = data_file.read()
@@ -36,6 +39,32 @@ class POSCorpus(object):
       corpus.sentences.append(sentence)
 
     return corpus
+  
+
+  @classmethod
+  def create_from_bnc(cls, data_file_path):
+    """Initialize corpus from the root directory of BNC corpus"""
+    corpus = POSCorpus()
+    bnc_reader = nltk.corpus.reader.bnc.BNCCorpusReader(root=data_file_path, fileids=r'[a-z]{3}/\w*\.xml')
+
+    # bnc_reader.sents(): list of [word1, word2, ...]
+    # bnc_reader.tagged_sents(): list of [(lemma1, pos1), (lemma2, pos2), ...]
+    # There is no way to get all three at the same time, so we must zip and traverse both lists simultaneously.
+    sents = bnc_reader.sents()
+    tagged_sents = bnc_reader.tagged_sents(stem=True)
+
+    corpus.sentences = []
+    for sent, tagged_sent in tqdm.tqdm(zip(sents, tagged_sents)):
+      sentence = []
+      for word, (lemma, pos) in zip(sent, tagged_sent):
+        # Replace SUBST -> NOUN to be consistent with UD tags
+        if pos == 'SUBST':
+          pos = 'NOUN'
+        sentence.append({'word': word, 'lemma': lemma, 'pos': pos})
+      corpus.sentences.append(sentence)
+
+    return corpus
+
 
   def get_per_lemma_stats(self, flexibility_threshold=0.05):
     # Gather usages of each lemma
