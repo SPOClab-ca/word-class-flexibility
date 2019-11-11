220f5be659c661e1838557818465051d56b193d8
Bai Li <bai@cs.toronto.edu>
2019-11-11 14:55:31 -0500

    Change ELMo visualization notebook to include BERT

--- notebooks/ELMoPlusBERT.py
+++ notebooks/ELMoPlusBERT.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # ELMo Contextual Embeddings
+# # ELMo and BERT Contextual Embeddings
 # 
-# In this notebook, we use contextual embeddings from ELMo to study semantic change of conversion.
+# In this notebook, we use contextual embeddings from ELMo/BERT to study semantic change of conversion.
 
 # In[1]:
 
@@ -38,13 +38,13 @@ BNC_FILE = "../data/bnc/bnc.pkl"
 corpus = src.corpus.POSCorpus.create_from_bnc_pickled(data_file_path=BNC_FILE)
 
 
-# ## Run ELMo on random part of the corpus
+# ## Compute embeddings on random part of the corpus
 
-# In[3]:
+# In[ ]:
 
 
 # Take only 1M words out of 4M to make it run faster
-SAMPLE_PROPORTION = 0.25
+SAMPLE_PROPORTION = 1.0
 random.seed(12345)
 random_indices = random.sample(range(len(corpus.sentences)), int(SAMPLE_PROPORTION * len(corpus.sentences)))
 
@@ -53,22 +53,23 @@ for ix in random_indices:
   sampled_sentences.append(corpus.sentences[ix])
   
 embedder = src.semantic_embedding.SemanticEmbedding(sampled_sentences)
+embedder.init_bert(layer=12)
 
 
-# ## ELMo embeddings of instances of a fixed lemma
+# ## Compute embeddings of instances of a fixed lemma
 
-# In[6]:
+# In[4]:
 
 
-FIXED_LEMMA = "use"
-noun_embeddings, verb_embeddings = embedder.get_elmo_embeddings_for_lemma(FIXED_LEMMA)
+FIXED_LEMMA = "train"
+noun_embeddings, verb_embeddings = embedder.get_bert_embeddings_for_lemma(FIXED_LEMMA)
 print("Noun instances:", noun_embeddings.shape[0])
 print("Verb instances:", verb_embeddings.shape[0])
 
 
 # ## Apply PCA and plot
 
-# In[7]:
+# In[5]:
 
 
 pca = sklearn.decomposition.PCA(n_components=2)
@@ -77,67 +78,62 @@ all_embeddings_df = pd.DataFrame({'x0': all_embeddings[:,0], 'x1': all_embedding
 all_embeddings_df['pos'] = ['noun'] * len(noun_embeddings) + ['verb'] * len(verb_embeddings)
 
 
-# In[8]:
+# In[6]:
 
 
 plot = sns.scatterplot(data=all_embeddings_df, x='x0', y='x1', hue='pos')
-plot.set(title="ELMo embeddings for lemma: '%s'" % FIXED_LEMMA)
+plot.set(title="Contextual embeddings for lemma: '%s'" % FIXED_LEMMA)
 plt.show()
 
 
-# ## Utility to inspect what ELMo is capturing
-
-# In[10]:
-
-
+# ## Utility to inspect what it's capturing
 num_printed = 0
 for sentence_ix in range(len(sampled_sentences)):
   token_list = sampled_sentences[sentence_ix]
-  embeddings = embedder.elmo_embeddings[sentence_ix]
+  embeddings = embedder.bert_embeddings[sentence_ix]
   for i in range(len(token_list)):
     if token_list[i]['lemma'] == FIXED_LEMMA:
       v = pca.transform(embeddings[i][np.newaxis, :])[0]
-      if v[1] < -10: # <- Put whatever condition here
+      if 1 < v[0] < 2: # <- Put whatever condition here
         print(v)
         print(' '.join([t['word'] for t in token_list]))
         print()
         num_printed += 1
   if num_printed > 10:
     break
-
-
 # ## Cosine similarity between noun and verb usages
 
-# In[11]:
+# In[27]:
 
 
 lemma_count_df = corpus.get_per_lemma_stats()
 
 # Filter: must have at least [x] noun and [x] verb usages
-lemma_count_df = lemma_count_df[(lemma_count_df['noun_count'] >= 100) & (lemma_count_df['verb_count'] >= 100)]
+lemma_count_df = lemma_count_df[(lemma_count_df['noun_count'] >= 80) & (lemma_count_df['verb_count'] >= 80)]
 lemma_count_df = lemma_count_df.sort_values('total_count', ascending=False)
+lemma_count_df = lemma_count_df[~lemma_count_df.lemma.isin(['go', 'will', 'may'])]
 print('Remaining lemmas:', len(lemma_count_df))
 
 
-# In[13]:
+# In[ ]:
 
 
-lemma_count_df['nv_cosine_similarity'] =   lemma_count_df.apply(lambda row: embedder.get_nv_cosine_similarity(row.lemma), axis=1)
+lemma_count_df['nv_cosine_similarity'] =   lemma_count_df.apply(lambda row: embedder.get_contextual_nv_similarity(row.lemma, method="bert"), axis=1)
 
 
-# In[14]:
+# In[29]:
 
 
 lemma_count_df[['lemma', 'noun_count', 'verb_count', 'majority_tag', 'nv_cosine_similarity']]   .sort_values('nv_cosine_similarity').head(8)
 
 
-# In[15]:
+# In[30]:
 
 
 lemma_count_df[['lemma', 'noun_count', 'verb_count', 'majority_tag', 'nv_cosine_similarity']]   .sort_values('nv_cosine_similarity', ascending=False).head(8)
 
 
-# In[16]:
+# In[31]:
 
 
 plot = sns.distplot(lemma_count_df[lemma_count_df.majority_tag == 'NOUN'].nv_cosine_similarity, label='Base=N')
@@ -148,7 +144,7 @@ plot.set(title="Average Cosine Similarity between Noun/Verb Usage",
 plt.show()
 
 
-# In[17]:
+# In[32]:
 
 
 # T-test of difference in mean
