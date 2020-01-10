966a9cd48713d5341756838e74e1199c025ae494
Bai Li <bai@cs.toronto.edu>
2020-01-10 16:26:03 -0500

    Fix total token calculation, parallelize processing across languages

--- notebooks/MultilingualUD.py
+++ notebooks/MultilingualUD.py
@@ -5,7 +5,7 @@
 # 
 # Compute noun/verb frequency-based statistics for all languages in UD
 
-# In[71]:
+# In[1]:
 
 
 import sys
@@ -15,6 +15,7 @@ import glob
 import os
 from collections import defaultdict
 import pandas as pd
+import multiprocessing as mp
 
 import src.corpus
 
@@ -37,7 +38,7 @@ for ud_corpus_name in os.listdir(UD_PATH):
     ud_files[language_name].append(conllu_file)
 
 
-# In[114]:
+# In[3]:
 
 
 ud_files['French'][:5]
@@ -45,20 +46,27 @@ ud_files['French'][:5]
 
 # ## All UD files in one language
 
-# In[94]:
+# In[4]:
 
 
 corpus = src.corpus.POSCorpus.create_from_ud(data_file_list=ud_files['English'])
 
 
-# In[109]:
+# In[5]:
 
 
 lemma_count_df = corpus.get_per_lemma_stats()
 lemma_count_df.sort_values('total_count', ascending=False).head(10)
 
 
-# In[110]:
+# In[6]:
+
+
+total_tokens = sum([len(sentence) for sentence in corpus.sentences])
+print('Total tokens:', total_tokens)
+
+
+# In[7]:
 
 
 # Only consider lemmas with at least 10 usages
@@ -69,20 +77,14 @@ noun_flexibility = len(lemma_count_df[(lemma_count_df['majority_tag'] == 'NOUN')
 verb_flexibility = len(lemma_count_df[(lemma_count_df['majority_tag'] == 'VERB') & (lemma_count_df['is_flexible'])]) / verb_lemmas
 
 
-# In[111]:
-
-
-print('Total tokens:', lemma_count_df.total_count.sum())
-
-
-# In[112]:
+# In[8]:
 
 
 print('Noun lemmas with >= 10 usages:', noun_lemmas)
 print('Verb lemmas with >= 10 usages:', verb_lemmas)
 
 
-# In[113]:
+# In[9]:
 
 
 print('Noun Flexibility = P(flexible | noun):', noun_flexibility)
@@ -94,44 +96,50 @@ print('Verb Flexibility = P(flexible | verb):', verb_flexibility)
 # In[ ]:
 
 
-all_language_stats = pd.DataFrame([], columns=['language', 'tokens', 'noun_lemmas', 'verb_lemmas', 'noun_flexibility', 'verb_flexibility'])
-for language_name, language_ud_list in ud_files.items():
-  print(language_name)
+def process_ud_language(args):
+  language_name, language_ud_list = args
+  print('Processing:', language_name)
   
   corpus = src.corpus.POSCorpus.create_from_ud(data_file_list=ud_files[language_name])
-  if len(corpus.sentences) == 0: continue
+  if len(corpus.sentences) == 0: return None
+  total_tokens = sum([len(sentence) for sentence in corpus.sentences])
   lemma_count_df = corpus.get_per_lemma_stats()
   lemma_count_df = lemma_count_df[lemma_count_df['total_count'] >= 10].sort_values('total_count', ascending=False)
   noun_lemmas = len(lemma_count_df[lemma_count_df['majority_tag'] == 'NOUN'])
   verb_lemmas = len(lemma_count_df[lemma_count_df['majority_tag'] == 'VERB'])
-  if noun_lemmas == 0 or verb_lemmas == 0: continue
+  if noun_lemmas == 0 or verb_lemmas == 0: return None
   noun_flexibility = len(lemma_count_df[(lemma_count_df['majority_tag'] == 'NOUN') & (lemma_count_df['is_flexible'])]) / noun_lemmas
   verb_flexibility = len(lemma_count_df[(lemma_count_df['majority_tag'] == 'VERB') & (lemma_count_df['is_flexible'])]) / verb_lemmas
   
-  all_language_stats.loc[len(all_language_stats)] = {
+  return pd.Series({
     'language': language_name,
-    'tokens': lemma_count_df.total_count.sum(),
+    'tokens': total_tokens,
     'noun_lemmas': noun_lemmas,
     'verb_lemmas': verb_lemmas,
     'noun_flexibility': noun_flexibility,
     'verb_flexibility': verb_flexibility,
-  }
+  })
+
+pool = mp.Pool()
+results = pool.map(process_ud_language, ud_files.items())
+results = [r for r in results if r is not None]
+all_language_stats = pd.DataFrame(results)
 
 
-# In[106]:
+# In[27]:
 
 
 all_language_stats = all_language_stats.sort_values('tokens', ascending=False)
 all_language_stats
 
 
-# In[107]:
+# In[28]:
 
 
 all_language_stats.to_csv('multi-language-ud.csv', index=False)
 
 
-# In[108]:
+# In[29]:
 
 
 all_language_stats[(all_language_stats.noun_flexibility > 0.05) & (all_language_stats.verb_flexibility > 0.05)]
