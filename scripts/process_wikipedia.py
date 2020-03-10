569ad733d5cc34b854e66d62004d7ca7f6c52620
Bai Li <bai@cs.toronto.edu>
2020-03-09 23:42:44 -0400

    Finish script to combine Wikipedia with UD lemma merger

--- scripts/process_wikipedia.py
+++ scripts/process_wikipedia.py
@@ -1,11 +1,13 @@
-"""(Work in progress)
+"""
 Script to process multilingual Wikipedia files and save data into Python Pickle format.
 
 Usage:
   python scripts/process_wikipedia.py \
-    --dir=data/wiki/nl \
-    --to_file=data/processed/nl.pkl \
-    --sample 1000
+    --wiki_dir=data/wiki/ \
+    --ud_dir=data/ud_all/ud-treebanks-v2.5/ \
+    --dest_dir=data/processed/ \
+    --lang=nl \
+    --tokens 1000
 """
 
 import collections
@@ -16,20 +18,49 @@ import random
 import glob
 import tqdm
 import spacy_udpipe
+import src.corpus
 
-# Download language models
-#for lang in 'ar da en es fr he id it ja la nl nn pl ro sl zh'.split():
-#  spacy_udpipe.download(lang)
+LANGUAGES = {
+  'ar': 'Arabic',
+  'da': 'Danish',
+  'de': 'German',
+  'en': 'English',
+  'es': 'Spanish',
+  'fr': 'French',
+  'he': 'Hebrew',
+  'id': 'Indonesian',
+  'it': 'Italian',
+  'ja': 'Japanese',
+  'la': 'Latin',
+  'nl': 'Dutch',
+  'nn': 'Norwegian',
+  'pl': 'Polish',
+  'ro': 'Romanian',
+  'sl': 'Slovenian',
+  'zh': 'Chinese',
+}
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dir', type=str)
-parser.add_argument('--to_file', type=str)
-parser.add_argument('--sample', type=int)
+parser.add_argument('--wiki_dir', type=str)
+parser.add_argument('--ud_dir', type=str)
+parser.add_argument('--dest_dir', type=str)
+parser.add_argument('--lang', type=str)
+parser.add_argument('--tokens', type=int)
 args = parser.parse_args()
 print(args)
 
+# Download language models
+spacy_udpipe.download(args.lang)
+
+# Process UD to get lemma mappings
+lang_name_full = LANGUAGES[args.lang]
+ud_treebanks = src.corpus.group_treebanks_by_language(args.ud_dir)[lang_name_full]
+print('Processing UD:', lang_name_full)
+ud_corpus = src.corpus.POSCorpus.create_from_ud(data_file_list=ud_treebanks)
+
+# Process Wikipedia
 ALL_LINES = []
-for fname in glob.glob(args.dir + '/**/*'):
+for fname in glob.glob(args.wiki_dir + '/' + args.lang + '/**/*'):
   print(fname)
   with open(fname) as f:
     contents = f.read()
@@ -41,12 +72,10 @@ for fname in glob.glob(args.dir + '/**/*'):
     ALL_LINES.append(line)
 
 print('Lines:', len(ALL_LINES))
+random.seed(12345)
+random.shuffle(ALL_LINES)
 
-if args.sample:
-  random.seed(12345)
-  ALL_LINES = random.sample(ALL_LINES, k=args.sample)
-
-nlp = spacy_udpipe.load("en")
+nlp = spacy_udpipe.load(args.lang)
 def process_line(line):
   return nlp(line)
 
@@ -54,13 +83,29 @@ pool = multiprocessing.Pool()
 token_count = 0
 pos_counts = collections.defaultdict(int)
 
-for doc in tqdm.tqdm(pool.imap(process_line, ALL_LINES), total=len(ALL_LINES)):
+sentences = []
+for doc in tqdm.tqdm(pool.imap(process_line, ALL_LINES)):
+  if token_count > args.tokens:
+    break
   for sent in doc.sents:
+    if len(sent) < 5:
+      continue
+    sentence = []
     for token in sent:
       token_count += 1
-      #print(token.text, token.pos_)
+      # Try using UD lemma merger, otherwise fallback to udpipe lemma output
+      token_lemma = ud_corpus.get_merged_lemma_for_word(token.text.lower())
+      if token_lemma is None:
+        token_lemma = token.lemma_
+      sentence.append({'word': token.text, 'lemma': token_lemma, 'pos': token.pos_})
       pos_counts[token.pos_] += 1
+    sentences.append(sentence)
 
 print('Tokens:', token_count)
 for tok, tokcount in pos_counts.items():
   print('%s: %d' % (tok, tokcount))
+
+save_file = args.dest_dir + '/' + args.lang + '.pkl'
+print('Saving:', save_file)
+with open(save_file, 'wb') as f:
+  pickle.dump(sentences, f)
